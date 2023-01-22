89be219de9874d1b2b08949f2b6bfc59e7e37f85
Vlad Makhnenko <vlmakh@gmail.com>
2023-01-22 16:17:52 +0200

    update main img

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -7,7 +7,13 @@ export const HomePage = () => {
       <p>
         <b>Please login to watch your contacts</b>
       </p>
-      <img width="360" src={phonebook} alt="phonebook" loading="lazy" />
+      <img
+        width="360"
+        height="360"
+        src={phonebook}
+        alt="phonebook"
+        loading="lazy"
+      />
     </HomeBox>
   );
 };
