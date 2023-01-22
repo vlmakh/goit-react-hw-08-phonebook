7ee33fd4ad53bd5d058e3febc86153dca3805af6
Vlad Makhnenko <vlmakh@gmail.com>
2023-01-22 05:42:28 +0200

    refactor capitalize func

--- src/utils/capitalize.js
+++ src/utils/capitalize.js
@@ -1,9 +1,6 @@
 export function capitalize(string) {
-  const arr = string.split(' ');
-
-  for (let i = 0; i < arr.length; i++) {
-    arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
-  }
-
-  return arr.join(' ');
+  return string
+    .split(' ')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
 }
